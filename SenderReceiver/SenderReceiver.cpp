eaba5a7c341be92f0e5085c4489e8ab50ba2711c
skandinaff <sevalks@gmail.com>
2019-02-11 18:59:27 +0200

    Implemented prototype of a sender and a reciever simulation

--- SenderReceiver/SenderReceiver.cpp
+++ SenderReceiver/SenderReceiver.cpp
@@ -1,12 +1,88 @@
-// SenderReceiver.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <csignal>
+#include <windows.h>
+
+using namespace std;
+
+class Sensor {
+public:
+	string name;
+
+	void setName(string _name) {
+		name = _name;
+	}
+	string getName(void) {
+		return name;
+	}
+	double measure(void) {
+		return (rand() % 100) + 1;
+	}
+};
+
+class Receiver {
+public:
+	string name;
+	int numberOfMsgs = 0;
+	Receiver(string _sensor_name) {
+		name == _sensor_name;
+	}
+
+	void incrementN(void) {
+		numberOfMsgs += 1;
+	}
+
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	srand(time(NULL));
+
+
+
+	Sensor sensor1;
+	Sensor sensor2;
+	sensor1.setName("Sensor 1");
+	sensor1.setName("Sensor 2");
+
+	Receiver receiver1("Sensor 1");
+	Receiver receiver2("Sensor 2");
+
+	SHORT key = 0;
+
+
+	double data;
+	
+		while (key == 0) {
+
+		cout << "Sensor 1 send: ";
+		data = sensor1.measure();
+		cout << data << endl;
+		if (data != NULL) receiver1.incrementN();
+
+		cout << "Sensor 2 send: ";
+		data = sensor2.measure();
+		cout << data << endl;
+		if (data != NULL) receiver2.incrementN();
+		key = GetKeyState('A');
+		Sleep(100);
+
+	}
+
+	cout << "Programm stopped" << endl;
+	cout << "Receiver 1 got: " << receiver1.numberOfMsgs << "Messages" << endl;
+	cout << "Receiver 2 got: " << receiver2.numberOfMsgs << "Messages" << endl;
+	system("pause");
+	return 0;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
